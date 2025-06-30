e775f6abd70b617d32da8b69e595417cb09f3397
Pim Sanders <pimmiesanders@gmail.com>
2025-06-30 09:07:31 +0200

    changes

--- tests/plugins/os/windows/test_searchindex.py
+++ tests/plugins/os/windows/test_searchindex.py
@@ -1,48 +1,29 @@
 from __future__ import annotations
 
-import io
-from pathlib import Path
 from typing import TYPE_CHECKING
-from unittest.mock import Mock, mock_open, patch
 
-import pytest
-
-from dissect.target.exceptions import UnsupportedPluginError
 from dissect.target.filesystem import VirtualFilesystem
 from dissect.target.helpers.regutil import VirtualHive, VirtualKey
 from dissect.target.plugins.os.windows.searchindex import SearchIndexPlugin
-
 from tests._utils import absolute_path
 
 if TYPE_CHECKING:
+    from dissect.target.filesystem import VirtualFilesystem
     from dissect.target.target import Target
 
-@pytest.mark.parametrize(
-    ("target", "fs_win", "target_file"),
-    [
-        (
-            "target_win_users",
-            "fs_win",
-            "\\sysvol\\Microsoft\\Search\\Data\\Applications\\Windows\\Windows.db",
-        ),
-    ],
-)
-def test_searchindex_db(target: Target, fs_win: VirtualFilesystem, target_file: str, request: pytest.FixtureRequest, hive_hklm: VirtualHive) -> None:
+
+def test_searchindex_db(target_win: Target, fs_win: VirtualFilesystem, hive_hklm: VirtualHive) -> None:
     key_name = "SOFTWARE\\Microsoft\\Windows Search"
     key = VirtualKey(hive_hklm, key_name)
     key.add_value("DataDirectory", "%ProgramData%\\Microsoft\\Search\\Data\\")
     hive_hklm.map_key(key_name, key)
 
-
     searchindex_file = absolute_path("_data/plugins/os/windows/searchindex/Windows.db")
-    if target_file.startswith("\\sysvol\\"):
-        target_file = target_file.replace("\\sysvol\\", "C:\\ProgramData\\")
-    fs_win.map_file(target_file, searchindex_file)
+    fs_win.map_file("sysvol/Programdata/Microsoft/Search/Data/Applications/Windows/Windows.db", searchindex_file)
 
+    target_win.add_plugin(SearchIndexPlugin)
 
-    target.add_plugin(SearchIndexPlugin)
-
-    records = list(target.searchindex())
+    records = list(target_win.searchindex())
 
     # assert len(records) == 3
     assert records[0].filename == "C:/ProgramData/Microsoft/Windows/Start Menu"
